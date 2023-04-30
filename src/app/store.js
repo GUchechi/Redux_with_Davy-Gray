92b62ca9db22308dd6bdaf4be0c6c3fcc03d01ef
GUchechi <godswilluchendu7@gmail.com>
2023-04-30 19:43:21 +0100

    userSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../features/posts/postsSlice";
-import usersReducer from "../features/users/usersSlice";
+import usersReducer from "../features/users/userSlice";
 
 export const store = configureStore({
   reducer: {
