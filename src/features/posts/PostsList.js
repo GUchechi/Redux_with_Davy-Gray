e142452c17383bc4e2562b506816807d8828bc51
GUchechi <godswilluchendu7@gmail.com>
2023-04-30 20:03:16 +0100

    postAuthor

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 
 import React from "react";
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "../../../PostAuthor";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
@@ -10,6 +11,9 @@ const PostsList = () => {
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId}/>
+      </p>
     </article>
   ));
   return (
