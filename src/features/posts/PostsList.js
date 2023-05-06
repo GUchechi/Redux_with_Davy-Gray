36dca99d8cacfd68e6c3075563ed2bc1c59adc47
GUchechi <godswilluchendu7@gmail.com>
2023-05-06 19:48:26 +0100

    postList

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -6,9 +6,6 @@ import {
   getPostsError,
   fetchPosts,
 } from "./postsSlice";
-import PostAuthor from "../posts/PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
   const dispatch = useDispatch();
@@ -27,15 +24,7 @@ const PostsList = () => {
     .sort((a, b) => b.date.localeCompare(a.date));
 
   const renderPosts = orderedPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
+    
   ));
   return (
     <section>
