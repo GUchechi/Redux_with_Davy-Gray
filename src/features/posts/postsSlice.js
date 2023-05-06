4bbd8fb61f0185f10747867e838c847696dfc79f
GUchechi <godswilluchendu7@gmail.com>
2023-05-06 19:38:02 +0100

    postSlice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -83,6 +83,7 @@ const postsSlice = createSlice({
 
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
