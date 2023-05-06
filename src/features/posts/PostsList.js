c1da0c62093647d85b84d5520372e2c69fce64fd
GUchechi <godswilluchendu7@gmail.com>
2023-05-06 19:50:49 +0100

    isLoading

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -6,6 +6,7 @@ import {
   getPostsError,
   fetchPosts,
 } from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
   const dispatch = useDispatch();
@@ -19,17 +20,23 @@ const PostsList = () => {
     }
   }, [postStatus, dispatch]);
 
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
-
-  const renderPosts = orderedPosts.map((post) => (
-    
-  ));
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
   return (
     <section>
       <h2>Posts</h2>
-      {renderPosts}
+      {content}
     </section>
   );
 };
