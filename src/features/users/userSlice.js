6300cc647487ff93a3ba7fb94d2cb9f7fa989ea8
GUchechi <godswilluchendu7@gmail.com>
2023-05-07 11:18:13 +0100

    userSlice

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -5,6 +5,11 @@ const USERS_URL = "https://jsonplaceholder.typicode.com/users";
 
 const initialState = [];
 
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data;
+});
+
 const usersSlice = createSlice({
   name: "users",
   initialState,
