f8fe5f70294513779814a717354e853527996666
GUchechi <godswilluchendu7@gmail.com>
2023-05-08 16:03:09 +0100

    optimization

--- src/features/posts/PostExcerpt.js
+++ src/features/posts/PostExcerpt.js
@@ -1,10 +1,10 @@
+import React from "react";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 import { Link } from "react-router-dom";
 
-
-const PostsExcerpt = ({ post }) => {
+let PostsExcerpt = ({ post }) => {
   return (
     <article>
       <h2>{post.title}</h2>
@@ -18,4 +18,7 @@ const PostsExcerpt = ({ post }) => {
     </article>
   );
 };
+
+PostsExcerpt = React.memo(PostsExcerpt);
+
 export default PostsExcerpt;
