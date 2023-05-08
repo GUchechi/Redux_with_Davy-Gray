015c31b77b566fcc7e0149a4f37a13872ab2bcd3
GUchechi <godswilluchendu7@gmail.com>
2023-05-08 15:22:49 +0100

    userPage

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,15 +1,17 @@
 import { useSelector } from "react-redux";
-import { selectUserById } from "../users/usersSlice";
-import { selectAllPosts, selectPostsByUser } from "../posts/postsSlice";
+import { selectUserById } from "../users/userSlice";
+// import {selectPostsByUser } from "../posts/postsSlice";
 import { Link, useParams } from "react-router-dom";
+import { selectAllPosts } from "../posts/postsSlice";
 
 const UserPage = () => {
   const { userId } = useParams();
   const user = useSelector((state) => selectUserById(state, Number(userId)));
 
-  const postsForUser = useSelector((state) =>
-    selectPostsByUser(state, Number(userId))
-  );
+  const postsForUser = useSelector((state) => {
+    const allPosts = selectAllPosts(state);
+    return allPosts.filter((post) => post.userId === Number(userId));
+  });
 
   const postTitles = postsForUser.map((post) => (
     <li key={post.id}>
