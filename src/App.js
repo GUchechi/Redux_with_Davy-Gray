3826b980c38fac9746be75ac182f3d0fb8b350c9
GUchechi <godswilluchendu7@gmail.com>
2023-05-08 13:33:19 +0100

    app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import PostsList from "./features/posts/PostsList";
 import SinglePostPage from "./features/posts/SinglePostPage";
 // import EditPostForm from "./features/posts/EditPostForm";
 import Layout from "./components/Layout";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <main className="App">
-      <AddPostForm />
-      <PostsList />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          {/* <Route path="edit/:postId" element={<EditPostForm />} /> */}
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
