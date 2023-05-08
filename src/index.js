a1a61b479a7d7efee93a892a576425446a985a6c
GUchechi <godswilluchendu7@gmail.com>
2023-05-08 13:25:55 +0100

    singlePost

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { fetchUsers } from "./features/users/userSlice";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 store.dispatch(fetchUsers());
 
